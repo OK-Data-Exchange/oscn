9252e28c052fadf3a80d6441169638328943a42e
Sabrina Leggett <sabrina.gelbart@gmail.com>
2025-05-09 10:32:28 -0500

    working

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Issue, type: :model do
   describe 'associations' do
-    it { should belong_to(:count_code) }
+    it { should belong_to(:count_code).optional }
     it { should belong_to(:court_case) }
     it { should have_many(:issue_parties).dependent(:destroy) }
   end
---
+++ spec/services/scrapers/daily_filings_alt_counties_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
+
+RSpec.describe Scrapers::DailyFilingsAltCounties do
+  describe '#perform' do
+    before do
+      create(:county, name: 'Muskogee')
+      create(:case_type, :felony)
+    end
+
+    it 'creates a new case' do
+      VCR.use_cassette 'daily_filings_alt_counties' do
+        Sidekiq::Testing.inline! do
+          Scrapers::DailyFilingsAltCounties.perform('Muskogee', '2010-01-04')
+          expect(CourtCase.first).not_to eq(nil)
+          expect(CourtCase.first.html).not_to eq(nil)
+        end
+      end
+    end
+
+    it 'scrapes case but not html when equeue is false' do
+      VCR.use_cassette 'daily_filings_alt_counties' do
+        Sidekiq::Testing.inline! do
+          Scrapers::DailyFilingsAltCounties.perform('Muskogee', '2010-01-04', enqueue: false)
+          expect(CourtCase.first).not_to eq(nil)
+          expect(CourtCase.first.case_html).to eq(nil)
+        end
+      end
+    end
+  end
+end
