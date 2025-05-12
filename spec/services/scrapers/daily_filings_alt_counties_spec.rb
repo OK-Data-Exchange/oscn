1ab65a2bfbff48abeec720e0cd4a08fe4aef14c4
Sabrina Leggett <sabrina.gelbart@gmail.com>
2025-05-12 10:12:42 -0500

    set user agent env var

--- spec/services/scrapers/daily_filings_alt_counties_spec.rb
+++ spec/services/scrapers/daily_filings_alt_counties_spec.rb
@@ -5,6 +5,7 @@ Sidekiq::Testing.fake!
 RSpec.describe Scrapers::DailyFilingsAltCounties do
   describe '#perform' do
     before do
+      ENV["USER_AGENT"] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
       create(:county, name: 'Muskogee')
       create(:case_type, :felony)
     end
